35facdcb66047c2dc286f4f54a1a563f6d0b2f12
Humberto de Oliveira <humbertosilva06@gmail.com>
2022-07-19 11:37:50 -0300

    exercicios 007 resolvidos

---
+++ exercicio3/script.js
@@ -0,0 +1,13 @@
+let a = 30
+let b = 60
+let c 
+console.log("o valor de A é: ", a)
+console.log("o valor de B é: ", b)
+c=a
+a=b
+b=c 
+console.log ("após alogica de inversão dos valores, o resultado é")
+console.log("o valor de A é: ", a)
+console.log("o valor de B é: ", b)
+
+/*lembrar sempre que o programa le linha por linha, de cima pra baixo. ele vai ler cada variavel e exibir, depois fazer as trocas ele exibira os valores trocados*/
